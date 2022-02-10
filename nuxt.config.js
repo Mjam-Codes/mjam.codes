2a09f875fe130fd37466e6a83eed4fcb68d3e44a
milanbargiel <milan.bargiel@googlemail.com>
2022-02-10 16:59:38 +0100

    Add favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,8 +3,9 @@ export default {
   target: 'static',
 
   // Global page headers: https://go.nuxtjs.dev/config-head
+  // Favicons created with https://favicon.io/
   head: {
-    title: 'mjam.codes',
+    title: 'Digital products | Mjam',
     htmlAttrs: {
       lang: 'en',
     },
@@ -14,7 +15,12 @@ export default {
       { hid: 'description', name: 'description', content: '' },
       { name: 'format-detection', content: 'telephone=no' },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [
+      { rel: 'apple-touch-icon', sizes: '180x180', href: '/favicon/apple-touch-icon.png' },
+      { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon/favicon-32x32.png' },
+      { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon/favicon-16x16.png' },
+      { rel: 'manifest', href: '/favicon/site.webmanifest' },
+    ],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
