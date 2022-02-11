579a80ad11560f4e9a02db3b85796b0478609f4a
milanbargiel <milan.bargiel@googlemail.com>
2022-02-11 08:32:27 +0100

    Change text in hero

--- components/MyHero.vue
+++ components/MyHero.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container p-20 text-center font-medium text-4xl md:text-3xl">
     <span class="text-custom-primary">
-      Mjam Codes</span> builds lightweight web <br>
+      Mjam</span> builds lightweight web <br>
       infrastructures for your designs. Our code is <br>
       reusable, modular, and responsive.
   </div>
