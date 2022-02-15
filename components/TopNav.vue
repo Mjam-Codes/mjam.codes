c441f310a76510ff64e57731a5c1b6859bb1d382
milanbargiel <milan.bargiel@googlemail.com>
2022-02-15 15:13:42 +0100

    Add hover styles for top nav

--- components/TopNav.vue
+++ components/TopNav.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="container nav mx-auto py-5 px-7">
     <ul class="flex justify-end gap-4 md:gap-8 font-medium">
-      <li class="">
+      <li class="hover:cursor-pointer hover:text-custom-secondary-500">
         <a href="https://github.com/Mjam-Codes" target="_blank">github</a>
       </li>
-      <li class="underline">
+      <li class="hover:cursor-pointer hover:text-custom-secondary-500">
         <a href="mailto:mail@mjam.codes">mail@mjam.codes</a>
       </li>
     </ul>
