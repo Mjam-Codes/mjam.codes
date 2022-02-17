1d0c6a35d1c6d1e3cf4e5522136df29bd1057e52
milanbargiel <milan.bargiel@googlemail.com>
2022-02-17 17:40:50 +0100

    Rename timesales project

--- components/MyShowcase.vue
+++ components/MyShowcase.vue
@@ -11,7 +11,7 @@ export default {
     return {
       projects: [
         { key: 'kulturgenerator', name: 'Kulturgenerator', description: 'An online art gallery based on Nuxtjs and the Strapi CMS.', imageUrl: 'kulturgenerator.png' },
-        { key: 'timesales', name: 'Timesales', description: 'A chatbot shop system build with Nuxtjs, Stripe and the Strapi CMS.', imageUrl: 'timesales.png' },
+        { key: 'timesales', name: 'Timesales Online', description: 'A chatbot shop system build with Nuxtjs, Stripe and the Strapi CMS.', imageUrl: 'timesales.png' },
         { key: 'buildingsocialecology', name: 'Building Social Ecology', description: 'An archive of socio-ecologic housing projects build with Wordpress.', imageUrl: 'building-social-ecology.png' }
       ]
     }
