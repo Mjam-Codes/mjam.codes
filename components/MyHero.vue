550820aeaa4c46ee630078df15519ab0948ce220
milanbargiel <milan.bargiel@googlemail.com>
2022-03-23 15:54:36 +0100

    Fix typo

--- components/MyHero.vue
+++ components/MyHero.vue
@@ -2,7 +2,7 @@
   <div
     class="container mx-auto max-w-2xl py-12 px-3 md:p-20 text-center font-medium text-4xl"
   >
-    Mjam builds lightweight web infrastructures for your designs. Our code is
+    Mjam builds lightweight web infrastructures for your design. Our code is
     reusable, modular, and responsive.
   </div>
 </template>
